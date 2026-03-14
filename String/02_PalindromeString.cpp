09b37b88055e91de47decb78ef17c1034fcb9284
Kundan Kumar <kundan027427@gmail.com>
2026-03-14 15:37:42 +0530

    Update 02_PalindromeString.cpp

--- String/02_PalindromeString.cpp
+++ String/02_PalindromeString.cpp
@@ -1,1 +1,38 @@
-
+/* T.C : O(n)
+  S.C  : O(1)
+*/
+class Solution {
+  public:
+    bool AlphNum(char ch){
+        if((ch >= '0' && ch <= '9') ||
+          (tolower(ch) >= 'a' && tolower(ch) <= 'z')){
+            return true;
+        }
+        
+        return false;
+    }
+    bool isPalindrome(string& s) {
+        // code here
+        int st = 0, end = s.length()-1;
+        while(st < end){
+            if(!AlphNum(s[st])){
+               st++; 
+               continue;
+            }
+            
+            if(!AlphNum(s[end])){
+               end--; 
+               continue;
+            }
+            if(tolower(s[st]) != tolower(s[end])){
+               return false;
+            }
+            
+            st++;
+            end--;
+        }
+        
+        return true;
+        
+    }
+};
