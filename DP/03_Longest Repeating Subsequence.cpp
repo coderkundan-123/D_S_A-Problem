f2b9447b12c44caa6ffc1b9e2cc469422a19e142
Kundan Kumar <kundan027427@gmail.com>
2026-03-16 04:52:18 +0530

    Update 03_Longest Repeating Subsequence.cpp

--- DP/03_Longest Repeating Subsequence.cpp
+++ DP/03_Longest Repeating Subsequence.cpp
@@ -1,1 +1,26 @@
-
+/***************** GFG PROBLEM *************/
+/* T.C : O(n*m)
+   S.C : O(n*m)
+*/
+class Solution {
+  public:
+    int t[1001][1001];
+    int LCS(string &s, int i, int j, int n){
+        if(i == n || j == n){
+            return 0;
+        }
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+        if(s[i] == s[j] && i != j){
+            return  t[i][j] = 1 + LCS(s, i+1 ,j+1, n);
+        }
+        return t[i][j]=  max(LCS(s, i+1, j, n), LCS(s, i, j+1, n));
+    }
+    int LongestRepeatingSubsequence(string &s) {
+        // Code here
+        int n = s.size();
+        memset(t, -1, sizeof(t));
+        return LCS(s, 0,0,n);
+    }
+};
