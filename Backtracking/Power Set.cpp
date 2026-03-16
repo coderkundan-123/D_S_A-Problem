084ecc3e98e7563b378f3a564a98d42853a22bae
Kundan Kumar <kundan027427@gmail.com>
2026-03-16 15:44:07 +0530

    Update Power Set.cpp

--- Backtracking/Power Set.cpp
+++ Backtracking/Power Set.cpp
@@ -1,1 +1,53 @@
+/*********** cpp ***********/
+/* T.C : O(2^n)
+   S.C :O(n * 2^n)
+*/
+class Solution {
+  public:
+    void RecAllpossible(string& s, int i, string prev, vector<string>& subsequance){
+        if(i == s.size()){
+            if(prev != "")// empty string 
+                subsequance.push_back(prev);
+            return;
+        }
+        //choose
+        RecAllpossible(s, i+1, prev+s[i],subsequance);
+        //reject
+        RecAllpossible(s, i+1, prev,subsequance);
+    }
+    vector<string> AllPossibleStrings(string s) {
+        // Code here
+        vector<string> subsequance;
+        
+        RecAllpossible(s, 0, "",subsequance);
+        sort(subsequance.begin(), subsequance.end());
+        return subsequance;
+    }
+};
 
+/********** sec Method *********/
+/* Bit - masking Apporch hai */
+class Solution {
+  public:
+    vector<string> AllPossibleStrings(string s) {
+        // Code here
+        vector<string>total_sub;
+        int n = s.size();
+        for(int i=0; i<pow(2,n); i++){
+            int curr = i;
+            string temp = "";
+            for(int i=0; i<=n; i++){
+                if(curr & 1 << i){
+                    temp +=s[i];
+                }
+            }
+           // reverse(temp.begin(), temp.end());
+            if(temp.size()){
+                total_sub.push_back(temp);
+            }
+        }
+        
+        sort(total_sub.begin(), total_sub.end());
+        return total_sub;
+    }
+};
