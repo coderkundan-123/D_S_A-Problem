dd3d7f349b1b6674ed1465b57a76777f4bfd28fb
Kundan Kumar <kundan027427@gmail.com>
2026-03-16 14:35:34 +0530

    Update Subsets.cpp

--- Backtracking/Subsets.cpp
+++ Backtracking/Subsets.cpp
@@ -1,1 +1,27 @@
+/********** cpp solution ***************/
+class Solution {
+public:
+    //T.C : O(n * 2^n)
+    void getAllSubsets(vector<int>& nums, vector<int>& ans, int i, vector<vector<int>>& allSubsets){
+        if(i == nums.size()){
+            allSubsets.push_back({ans});
+            return;
+        }
 
+        //include
+        ans.push_back(nums[i]);
+        getAllSubsets(nums, ans, i+1,allSubsets);
+
+        ans.pop_back();//backtrking
+
+        //exclusion
+        getAllSubsets(nums, ans, i+1,allSubsets);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+        getAllSubsets(nums, ans, 0, allSubsets);
+
+        return allSubsets;
+    }
+};
