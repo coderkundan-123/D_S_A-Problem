e8eb87d9ecfe0c75a76567437c92885454c451db
Kundan Kumar <kundan027427@gmail.com>
2026-03-17 16:52:22 +0530

    Update 17-March.cpp

--- LeetCode-Daily-Challenges/March/17-March.cpp
+++ LeetCode-Daily-Challenges/March/17-March.cpp
@@ -1,1 +1,75 @@
+/****************** first Apporch ************************/
+class Solution {
+public:
+    //T.C : O(m * nlogn)
+    //s.c : (m*n)
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int maxArea = 0;
+
+        for(int row = 0; row < m; row++){
+
+            for(int col = 0; col < n; col++){
+
+                if(matrix[row][col] == 1 && row > 0){
+                    matrix[row][col] += matrix[row-1][col];//cumulative 1s continue
+                }
+            }
+
+            vector<int> heights = matrix[row];
+            sort(begin(heights), end(heights), greater<int>());
+
+            for(int i = 0; i < n; i++){
+                int base = (i+1);
+                int height = heights[i];
+
+                maxArea = max(maxArea, base*height);
+            }
+        }
+
+        return maxArea;
+    }
+};
+
+/************** sec Apporch ***************/
+class Solution {
+public:
+    //T.C : O(m * nlogn)
+    //s.c : (m*n)
+    int largestSubmatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int maxArea = 0;
+
+        vector<int> prevRow(n,0);
+
+        for(int row = 0; row < m; row++){
+            vector<int> currRow = matrix[row];
+            for(int col = 0; col < n; col++){
+
+                if(currRow[col] == 1){
+                    currRow[col] += prevRow[col];//cumulative 1s continue
+                }
+            }
+
+            vector<int> heights = currRow;
+            sort(begin(heights), end(heights), greater<int>());
+
+            for(int i = 0; i < n; i++){
+                int base = (i+1);
+                int height = heights[i];
+
+                maxArea = max(maxArea, base*height);
+            }
+
+            prevRow = currRow;
+        }
+
+        return maxArea;
+    }
+};
+/*************** 3rd Apporch ****************/
 
