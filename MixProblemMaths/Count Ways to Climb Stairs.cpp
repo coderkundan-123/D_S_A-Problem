31f0354046f71bba9bdd0e55b06590ac1a41517e
Kundan Kumar <kundan027427@gmail.com>
2026-04-16 21:03:49 +0530

    Implement recursive function to count ways to climb stairs

--- MixProblemMaths/Count Ways to Climb Stairs.cpp
+++ MixProblemMaths/Count Ways to Climb Stairs.cpp
@@ -1,1 +1,17 @@
+#include<iostream>
+using namespace std;
 
+int countWay(int n){
+    if(n == 0 || n == 1){
+        return 1;
+    }
+
+    return countWay(n-1) + countWay(n-2);
+}
+
+int main(){
+    int num = 4;
+    int ans = countWay(num);
+    cout << ans << endl;
+    return 0;
+}
