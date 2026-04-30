445dc1986061259d9b1354a83e8daca5492e925e
Kundan Kumar <kundan027427@gmail.com>
2026-04-30 21:44:03 +0530

    Update cycle detection in undirected graph BFS and DFS.cpp

--- Graph/cycle detection in undirected graph BFS and DFS.cpp
+++ Graph/cycle detection in undirected graph BFS and DFS.cpp
@@ -40,3 +40,52 @@ class Solution {
         return false;
     }
 };
+/* BFS Methods */
+class Solution {
+public:
+    bool isCycleBFS(vector<vector<int>> &adj, int u, vector<bool> &vis){
+        queue<pair<int, int>> que;
+        que.push({u, -1});
+        vis[u] = true;
+
+        while(!que.empty()){
+            auto P = que.front();
+            que.pop();
+
+            int source = P.first;
+            int parent = P.second;
+
+            for(int v : adj[source]){
+                if(!vis[v]){
+                    vis[v] = true;
+                    que.push({v, source});
+                }
+                else if(v != parent){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    bool isCycle(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+
+        // build graph
+        for(auto &e : edges){
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
+        }
+
+        vector<bool> vis(V, false);
+
+        for(int i = 0; i < V; i++){
+            if(!vis[i]){
+                if(isCycleBFS(adj, i, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
