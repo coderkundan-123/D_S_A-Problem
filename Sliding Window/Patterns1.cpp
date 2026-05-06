1ea501100f385dfe3a46617c5c04315f36d256b3
Kundan Kumar <kundan027427@gmail.com>
2026-05-06 09:04:56 +0530

    Update Patterns1.cpp

--- Sliding Window/Patterns1.cpp
+++ Sliding Window/Patterns1.cpp
@@ -1,1 +1,25 @@
-
+/* Patterns 1 problem */
+/* constant window */
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        int sum = 0;
+        // first window of size k
+        for(int i = 0; i < k; i++){
+            sum += arr[i];
+        }
+        
+        int maxSum = sum;
+        
+        // sliding window
+        for(int i = k; i < n; i++){
+            sum += arr[i];        // add next element
+            sum -= arr[i - k];    // remove previous window element
+            maxSum = max(maxSum, sum);
+        }
+        
+        return maxSum;
+    }
+};
