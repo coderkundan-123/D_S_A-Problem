cae06e6ac19b805e84faf9f16f9de0e1e9b21ced
Kundan Kumar <kundan027427@gmail.com>
2026-08-18 00:02:44 +0530

    Update EqualSumPartition.cpp

--- DP/0-1- Knapsack/EqualSumPartition.cpp
+++ DP/0-1- Knapsack/EqualSumPartition.cpp
@@ -1,1 +1,42 @@
+// T.C = O(n * totalSum)
+// S.C = O(n * totalSum)
 
+/************************* Problem 02 ********************/
+class Solution {
+public:
+    bool subsetSum(vector<int>& nums, int sum){
+        int n = nums.size();
+        vector<vector<bool>>t(n+1, vector<bool>(sum+1, false));
+        // sum = 0 bossible to empty subset hai
+        for(int i=0; i<=n; i++){
+            t[i][0] = true; 
+        }
+        for(int j=1; j<=sum; j++){
+            t[0][j] = false;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(nums[i-1] <= j){
+                    t[i][j] = t[i-1][j-nums[i-1]] || t[i-1][j];
+                }else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+
+        return t[n][sum];
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+        }
+
+        if(sum % 2 !=0){
+            return false;
+        }
+        return subsetSum(nums, sum/2);
+    }
+};
